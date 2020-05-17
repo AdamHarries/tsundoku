6c9dac16170b7d070b4182368b052f87ba40b755
Adam Harries <adam.harries@codeplay.com>
2020-05-17 19:44:12 +0100

    Add documentation tests to datamodel module

--- src/tsundoku/datamodel.rs
+++ src/tsundoku/datamodel.rs
@@ -72,7 +72,30 @@ impl Database {
         Ok(rows)
     }
 
-    pub fn get_tag_id(&self, tag: String) -> Result<Vec<u8>> {
+    /// # Get a tag that exists
+    ///
+    /// ```
+    /// # use tsundoku::datamodel::{Database, Entry};
+    /// let db = Database::open_in_memory().unwrap();
+    /// db.add_tag("tag 0");
+    /// db.add_tag("tag 1");
+    /// db.add_tag("tag 2");
+    /// db.add_tag("tag 3");
+    /// db.add_tag("tag 4");
+    ///
+    /// let tag_id = db.get_tag_id("tag 3").unwrap();
+
+    /// assert_eq!(tag_id[0], 4);
+    /// ```
+    ///
+    /// # Get a tag that doesn't exist
+    /// ```
+    /// # use tsundoku::datamodel::{Database, Entry};
+    /// let db = Database::open_in_memory().unwrap();
+    /// let tag_id = db.get_tag_id("This tag doesn't exist");
+    /// assert_eq!(tag_id, Ok(vec![]));
+    /// ```
+    pub fn get_tag_id(&self, tag: &str) -> Result<Vec<u8>> {
         let mut stmt = self
             .conn
             .prepare("select tag_id from tags where tag == ?1")?;
@@ -80,6 +103,29 @@ impl Database {
         tag_iter.collect()
     }
 
+    /// # Test if a database contains a tag
+    /// ```
+    /// use tsundoku::datamodel::{Database, Entry};
+    /// let db = Database::open_in_memory().unwrap();
+    /// db.add_tag("tag 0").unwrap();
+    /// let tag_exists = db.contains_tag("tag 0").unwrap();
+    /// assert!(tag_exists);
+    /// ```
+    pub fn contains_tag(&self, tag: &str) -> Result<bool> {
+        self.get_tag_id(tag).map(|v| v.len() > 0)
+    }
+
+    /// # List tags
+    /// ```
+    /// # use tsundoku::datamodel::{Database, Entry};
+    /// let db = Database::open_in_memory().unwrap();
+    /// let tags: Vec<&str> = vec!["tag 0", "tag 1", "tag 2", "tag 3", "tag 4"];
+    /// for tag in &tags {
+    ///     db.add_tag(tag);
+    /// }
+    /// let db_tags = db.list_tags().unwrap();
+    /// assert_eq!(tags, db_tags);
+    /// ```
     pub fn list_tags(&self) -> Result<Vec<String>> {
         let mut stmt = self.conn.prepare("select tag from tags;")?;
         let tag_iter = stmt.query_map(params![], |row| Ok(row.get(0)?))?;
@@ -96,7 +142,7 @@ impl Database {
         )
     }
 
-    pub fn add_tag(&self, tag: String) -> Result<usize> {
+    pub fn add_tag(&self, tag: &str) -> Result<usize> {
         self.conn.execute(
             "insert into tags (tag_id, tag) values (NULL, ?1)",
             params![tag],
@@ -104,48 +150,36 @@ impl Database {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{Database, Entry};
-    #[test]
-    fn list_tags() {
-        let db = Database::open_in_memory().unwrap();
+// #[cfg(test)]
+// mod tests {
+//     use super::{Database, Entry};
 
-        let tags: Vec<String> = vec![
-            String::from("tag 0"),
-            String::from("tag 1"),
-            String::from("tag 2"),
-            String::from("tag 3"),
-            String::from("tag 4"),
-        ];
+//     // Helper methods
+//     fn add_tags_to_db(db: &Database) -> Vec<&str> {
+//         let tags: Vec<&str> = vec!["tag 0", "tag 1", "tag 2", "tag 3", "tag 4"];
 
-        for tag in &tags {
-            db.add_tag(tag.clone()).unwrap();
-        }
+//         for tag in &tags {
+//             db.add_tag(tag).unwrap();
+//         }
 
-        let db_tags = db.list_tags().unwrap();
+//         tags
+//     }
 
-        assert_eq!(tags, db_tags);
-    }
+//     #[test]
+//     fn get_existing_tag_id() {
+//         let db = Database::open_in_memory().unwrap();
+//         let _ = add_tags_to_db(&db);
+//         let tag_id = db.get_tag_id("tag 3").unwrap();
 
-    #[test]
-    fn get_existing_tag_id() {
-        let db = Database::open_in_memory().unwrap();
+//         assert_eq!(tag_id[0], 4);
+//     }
 
-        let tags: Vec<String> = vec![
-            String::from("tag 0"),
-            String::from("tag 1"),
-            String::from("tag 2"),
-            String::from("tag 3"),
-            String::from("tag 4"),
-        ];
+//     #[test]
+//     fn get_non_existing_tag_id() {
+//         let db = Database::open_in_memory().unwrap();
+//         let _ = add_tags_to_db(&db);
+//         let tag_id = db.get_tag_id("This tag doesn't exist");
 
-        for tag in &tags {
-            db.add_tag(tag.clone()).unwrap();
-        }
-
-        let tag_id = db.get_tag_id(String::from("tag 3")).unwrap();
-
-        assert_eq!(tag_id[0], 4);
-    }
-}
+//         assert_eq!(tag_id, Ok(vec![]));
+//     }
+// }
